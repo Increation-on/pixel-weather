7e6f64f2157a7c07b0634343b1fcbd3f5a7cf41e
Increation-on <maxim.d132213@gmail.com>
2025-12-31 13:06:42 +0300

    feat: add adaptive icon configuration

--- app.config.js
+++ app.config.js
@@ -5,9 +5,14 @@ export default {
     slug: "Pixel-weather",
     orientation: "portrait",
     userInterfaceStyle: "automatic", // Добавьте эту строку
+    icon: "./assets/icons/favicon/icon.png",
     android: {
       userInterfaceStyle: "automatic",
-      package: "com.learning.pixelweather", // Добавьте и эту строку для Android
+      package: "com.learning.pixelweather",
+      adaptiveIcon: {
+        foregroundImage: "./assets/icons/favicon/icon.png", // та же или другая
+        backgroundColor: "#FFFFFF" // или другой фон
+      } 
     },
     plugins: ["expo-router"],
     extra: {
